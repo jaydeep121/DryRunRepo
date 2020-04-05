0b62057185b933f7f2b3797aa8ec4856477bf7f4
jaydeep121 <jaydeepkr.pal@gmail.com>
2020-04-05 09:54:53 +0530

    basic array done

--- src/main/java/com/java/codingbat/ArrayBasic.java
+++ src/main/java/com/java/codingbat/ArrayBasic.java
@@ -2,14 +2,105 @@ package com.java.codingbat;
 
 public class ArrayBasic {
 	public static void main(String[] args) {
-		int arr[]= {100,2};
-		int b[]={8,9};
-		//System.out.println(start1(arr,b));
-		int ip[]=biggerTwo(arr,b);
+		int arr[]= {};
+		int b[]={2,5};
+		//System.out.println(frontPiece(arr));
+		int ip[]=front11(arr,b);
 		for(int n:ip) {
 			System.out.println(n);
 		}
 	}
+	public static int[] front11(int[] a, int[] b) {
+		  int arr[]=new int[2];
+		  if(a.length<1) {
+			  arr[0]=b[0];
+			  arr[1]=b[1];
+		  }
+		  else if(b.length<1) {
+			  arr[0]=a[0];
+			  arr[1]=a[1];
+		  }
+		  else {
+			  arr[0]=a[0];
+			  arr[1]=b[0];
+			  }
+		  return arr;
+	}
+	public static int[] make2(int[] a, int[] b) {
+		  int lenA=a.length;
+		  int make[]=new int[2];
+		  if(lenA>=2) {
+			  make[0]=a[0];
+			  make[1]=a[1];
+		  }
+		  else if(lenA==1) {
+			  make[0]=a[0];
+			  make[1]=b[0];
+		  }
+		  else {
+			  make[0]=b[0];
+			  make[1]=b[1];
+		  }
+		  return make;
+	}
+
+	public boolean unlucky1(int[] nums)
+	{
+		int lastP = nums.length-1;
+		if(lastP  >= 2)
+		{
+			if((nums[0] == 1 && nums[1] == 3) || (nums[1] == 1 && nums[2] == 3))
+				return true;
+			return (nums[lastP-1] == 1 && nums[lastP] == 3);
+		}
+		if(lastP  == 1)
+			return ((nums[0] == 1 && nums[1] == 3) || (nums[1] == 1 && nums[2] == 3));
+		return false;
+	}
+	public static int[] frontPiece(int[] nums) {
+		  if(nums.length>=2) {
+			  int a[]= {nums[0],nums[1]};
+			  return a;
+		  }
+		  else {
+			  return nums;
+		  }
+	}
+
+	public static int maxTriple(int[] nums) {
+		  int largest=nums[0];
+		  if(largest<nums[nums.length/2]) {
+			  largest=nums[nums.length/2];
+		  }
+		  if(largest<nums[nums.length-1]) {
+			  largest=nums[nums.length-1];
+		  }
+		  return largest;
+	}
+
+	public static int[] midThree(int[] nums) {
+		  int mid=nums.length/2;
+		  int arr[]= {nums[mid-1],nums[mid],nums[mid+1]};
+		  return arr;
+	}
+
+	public static int[] swapEnds(int[] nums) {
+		  int temp=nums[0];
+		  nums[0]=nums[nums.length-1];
+		  nums[nums.length-1]=temp;
+		  return nums;
+	}
+
+	public static int[] plusTwo(int[] a, int[] b) {
+		  int plusarr[]= {a[0],a[1],b[0],b[1]};
+		  return plusarr;
+	}
+
+	public static int[] makeMiddle(int[] nums) {
+		  int ar[]= {nums[(nums.length/2)-1],nums[(nums.length/2)]};
+		  return ar;
+	}
+
 	public static int[] biggerTwo(int[] a, int[] b) {
 //		if((a[0]+a[1]>b[0]+b[1])) {
 //			return a;
